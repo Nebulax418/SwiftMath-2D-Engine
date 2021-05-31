5066ef7baf16517c0116c4a160e36201af4b9003
Archisman Nath <61641517+Archi991-dot@users.noreply.github.com>
2021-05-31 10:18:42 -0400

    Delete Aegean2DMath directory

--- Aegean2DMath/Functionallity/ShapeAssets.java
+++
@@ -1,8 +0,0 @@
-package Aegean2DMath.Functionallity;
-
-public class ShapeAssets {
-
-    public static double squareOrRectangleArea(double length, double width) {
-        return length * width;
-    }
-}
